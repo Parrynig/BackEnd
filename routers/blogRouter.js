b6eb648ba01ebcb9b5c14683330a4c3a83736546
Parrying <paritoshn511@gmail.com>
2024-06-18 20:26:54 +0530

    readOperation

--- routers/blogRouter.js
+++ routers/blogRouter.js
@@ -3,22 +3,34 @@ const Model = require('../models/blogModel')
 
 const router = express.Router();
 
-router.post ('/add', (req, res) => {
-    
+router.post('/add', (req, res) => {
+
     console.log(req.body);
 
-    new Model (req.body).save()
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
+    new Model(req.body).save()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
     });
 
 
-});
+router.get('/getall', (req, res) =>{
+    // console.log(req.body);
 
-router.get ('/getall', (req, res) => {
-    res.send('Response from user GETALL');
-});
-module.exports = router ;
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.get('/getbytitle', )
+
+
+module.exports = router;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,22 +3,73 @@ const Model = require('../models/userModel')
 
 const router = express.Router();
 
-router.post ('/add', (req, res) => {
-    
+router.post('/add', (req, res) => {
+
     console.log(req.body);
 
-    new Model (req.body).save()
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    new Model(req.body).save()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 
 
 });
 
-router.get ('/getall', (req, res) => {
-    res.send('Response from user GETALL');
+// get all data from database
+
+router.get('/getall', (req, res) => {
+    // res.send('Response from user GETALL'); 
+
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
 });
-module.exports = router ;
+
+// ":" denotes url parameter
+// get all data using city
+router.get('/getbycity/:city', (req, res) => {
+    console.log(req.params.city);
+
+    Model.find({ city: req.params.city })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+});
+
+router.get('/getbyemail/:email', (req, res) => {
+
+    Model.findOne({ email: req.params.email })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/getbyid/:id', (req, res) => {
+
+    Model.findById( req.params.id )
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+
+module.exports = router;
